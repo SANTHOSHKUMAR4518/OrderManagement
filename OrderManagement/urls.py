5e487ec8547e483be0ccf01786c6c8cf429da89f
SANTHOSH KUMAR B S <bssanthoshkumar07@gmail.com>
2026-07-17 08:44:37 +0530

    Redirect home to dashboard

--- OrderManagement/urls.py
+++ OrderManagement/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
+def home(request):
+    return redirect('dashboard')
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orders.urls')),
--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 def home(request):
-    return render(request, "orders/home.html")
+    return redirect("dashboard")
 
 
 def product_list(request):
